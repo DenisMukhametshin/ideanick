67d3f044b7fbfb135a01f86c35f3f9ca5fe9518f
DenisMukhametshin <denis.mukhametshin@sharp-dev.net>
2025-08-23 15:45:01 +0700

    add trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
+
 app.get('/ping', (req, res) => {
   res.send('pong')
 })
+
+app.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }))
 
 app.listen(3000, () => {
   console.log('Listening on http://localhost:3000')
