0145628c167a7a496f9291ea37cb4357e5b6e595
DenisMukhametshin <denis.mukhametshin@sharp-dev.net>
2025-08-29 19:04:44 +0700

    add layouts and style lint

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TRPCProvider } from './lib/trpcProvider'
 import { AllIdeasPage } from './pages/AllIdeasPage'
+import * as routes from './lib/routes'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
+import { Layout } from './components/Layout'
+import './styles/global.scss'
+import { NewIdeaPage } from './pages/NewIdeaPage'
 
 export const App = () => {
   return (
     <TRPCProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
+            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </TRPCProvider>
   )
 }
